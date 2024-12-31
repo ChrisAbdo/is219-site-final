28bec519db2e9dac7cf7d57f43bb4b1f568c90d6
Chris Abdo <chris.j.abdo@gmail.com>
2024-12-31 07:00:16 -0500

    UPPERCASE

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistMono.className} antialiased px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 py-6 sm:py-12 md:py-16 lg:py-20 xl:py-24`}
+        className={`${GeistMono.className} antialiased px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 py-6 sm:py-12 md:py-16 lg:py-20 xl:py-24 uppercase`}
       >
         <div className="flex flex-col gap-8">
           <Nav />
